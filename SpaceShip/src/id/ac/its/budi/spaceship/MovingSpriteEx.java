5581604fae5120fee62922c768e28b1884b04534
boodboy1 <57314114+boodboy1@users.noreply.github.com>
2019-11-21 16:34:35 +0700

    Add files via upload

---
+++ SpaceShip/src/id/ac/its/budi/spaceship/MovingSpriteEx.java
@@ -0,0 +1,33 @@
+package id.ac.its.budi.spaceship;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class MovingSpriteEx extends JFrame {
+
+    public MovingSpriteEx() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+
+        add(new Board());
+
+        setTitle("Moving sprite");
+        setSize(400, 300);
+        pack();
+        
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            MovingSpriteEx ex = new MovingSpriteEx();
+            ex.setVisible(true);
+        });
+    }
+}
